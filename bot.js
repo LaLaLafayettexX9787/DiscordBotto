5ed7d4c0e767abb108d56daa4957e1192eda0bbc
Dianamond <55820045+Dianamond@users.noreply.github.com>
2020-01-06 12:29:55 -0500

    Add files via upload

---
+++ bot.js
@@ -0,0 +1,27 @@
+const Discord = require('discord.js');
+
+const client = new Discord.Client();
+
+const auth = require('./auth.json');
+
+
+
+client.on('ready', () => {
+
+    console.log(`Logged in as ${client.user.tag}!`);
+
+});
+
+
+client.on('message', msg => {
+
+    if (msg.content === 'ping') {
+  
+      msg.channel.send('pong');
+  
+    }
+  
+  });
+
+
+client.login(auth.token);
